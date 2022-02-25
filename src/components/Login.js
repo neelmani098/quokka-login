922ec463d9b2306a3a90028c5e3b159210c3bd36
Neelmani Singh <me@neelmani.in>
2022-02-25 09:32:12 +0530

    ref-final-styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import styles from "./Login.module.css";
 import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import Styles from "./Styles";
 
 import isEmail from "validator/es/lib/isEmail";
 
@@ -62,55 +63,47 @@ const Login = (props) => {
     <>
       <div className={styles.center_form}>
         <div className="w-full max-w-xs">
-          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <form className={Styles.form}>
             {!isValidCred && (
-              <p className="text-red-500 text-xs italic">
-                Incorrect Email Password
-              </p>
+              <p className={Styles.error}>Incorrect Email Password</p>
             )}
             <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="email"
-              >
+              <label className={Styles.email_label} htmlFor="email">
                 Email
               </label>
               <input
-                className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className={Styles.email_input}
                 id="email"
                 type="text"
                 placeholder="Email"
                 ref={emailInputRef}
               />
               {!formInputValidity.email && (
-                <p className="text-red-500 text-xs italic">
+                <p className={Styles.error}>
                   Please enter valid Email Address.
                 </p>
               )}
             </div>
             <div className="mb-6">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="password"
-              >
+              <label className={Styles.password_label} htmlFor="password">
                 Password
               </label>
               <input
-                className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className={Styles.password_input}
                 id="password"
                 type="password"
                 placeholder="******************"
                 ref={passwordInputRef}
               />
               {!formInputValidity.password && (
-                <p className="text-red-500 text-xs italic">
+                <p className={Styles.error}>
                   Please enter valid Password of 8 Characters or more.
                 </p>
               )}
             </div>
-            <div className="flex items-center justify-between">
+            <div className={Styles.button_div}>
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                className={Styles.button}
                 type="button"
                 onClick={onSubmitHandler}
               >
